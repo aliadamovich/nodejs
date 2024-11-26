d7200379e63be14db89511eef953ba01c2b25f62
aliadamovich <alesya.argentina@gmail.com>
2024-11-26 16:22:53 -0300

    init commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, {Request, Response} from 'express'
+import favicon from 'serve-favicon'
+import path from 'path'
+
+const app = express()
+const port = 3000
+app.use(express.static(path.join(__dirname, "public")))
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")))
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello Alesyia!!!')
+})
+app.get("/favicon.ico", (req, res) => {
+	res.sendFile(path.join(__dirname, "public", "favicon.ico"))
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening on port ${port}`)
+})
