1755b052f6f44d7274f8b35f4c98572fec24d4af
aliadamovich <alesya.argentina@gmail.com>
2024-11-26 16:47:29 -0300

    fixed bugs

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,8 @@ const app = express()
 
 const port = process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, "public")))
-app.use(favicon(path.join(__dirname, "public", "favicon.ico")))
 app.get('/', (req: Request, res: Response) => {
 	res.send('Hello Alesyia!!!')
-})
-app.get("/favicon.ico", (req, res) => {
-	res.sendFile(path.join(__dirname, "public", "favicon.ico"))
 })
 
 app.listen(port, () => {
