5b309c1c674b29aa39f255870b9c0a796f161de6
aliadamovich <alesya.argentina@gmail.com>
2024-12-09 16:49:35 -0300

    4

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,10 +1,7 @@
 import { MongoClient } from "mongodb";
 import mongoose from 'mongoose';
 
-const mongoUri =
-	process.env.MONGO_URL ||
-	"mongodb+srv://admin:789654@cluster0.fwwvp.mongodb.net/products?retryWrites=true&w=majority"
-// mongoose.connect('')
+const mongoUri = process.env.MONGO_URL || "mongodb://0.0.0.0:27017/?maxPoolSize=20&w=majority"
 
 export const client = new MongoClient(mongoUri);
 
